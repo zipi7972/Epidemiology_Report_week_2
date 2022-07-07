d15d687e9fc61f75cc6a8f9b1a6dc1ca3af8f9d1
zipiVinfeld <z4167972@gmail.com>
2022-07-07 13:04:56 +0300

    usingDal

--- Week_2/Week_2/Controllers/PatientController.cs
+++ Week_2/Week_2/Controllers/PatientController.cs
@@ -4,37 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http.Cors;
+using Week_2.DAL;
 using Week_2.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Week_2.Controllers
-{
+{   
+  
     [Route("api/[controller]")]
     [ApiController]
     public class PatientController : ControllerBase
     {
+        
         [HttpGet]
-        public  List<Report> GetAllReports()
+        public  List<Report> GetAllReport()
         {
-            List<Report> allReports = new List<Report>();
-            foreach (Patient patient in Data.Data.patients)
-            {
-                allReports.AddRange(patient.reports);
-            }
-            return allReports;
+            return patientDal.GetAllReports();
+           
         }
 
         //GET api/<PatientController>/5
         [HttpGet("{id}")]
         public List<Report> GetReportsById(int id)
         {
-            foreach (Patient patient in Data.Data.patients)
-            {
-                if (patient.id == id)
-                    return patient.reports;
-            }
-            return null;
+            return patientDal.GetReportsByIdPatient( id);
         }
 
         //[Route("api/city/[controller]")]
@@ -43,16 +37,7 @@ namespace Week_2.Controllers
 
         public List<Report> GetReportsByCity(string city)
         {
-            List<Report> reports = new List<Report>();
-            foreach (Patient patient in Data.Data.patients)
-            {
-                foreach (Report report in patient.reports)
-                {
-                    if (report.city == city)
-                        reports.Add(report);
-                }
-            }
-            return reports;
+            return patientDal.GetReportsByCity(city);
         }
 
         // POST api/<PatientController>
@@ -60,20 +45,7 @@ namespace Week_2.Controllers
         [Route("{patientId}")]
         public void PostReport([FromBody] Report report, int patientId)
         {
-            bool patientExit = false;
-            foreach (Patient patient in Data.Data.patients)
-            {
-                if(patient.id == patientId)
-                {
-                    patient.reports.Add(report);
-                    patientExit = true;
-                }
-            }
-            if (patientExit == false)
-            {
-                Patient newPatient = new Patient(new List<Report> { report }, patientId );
-                Data.Data.patients.Add(newPatient);
-            }
+            patientDal.AddReport(report, patientId);
         }
 
         // PUT api/<PatientController>/5
@@ -86,17 +58,7 @@ namespace Week_2.Controllers
         [HttpDelete("{patientId, ReportId}")]
         public void Delete(int patientId, int ReportId)
         {
-            foreach (Patient patient in Data.Data.patients)
-            {
-                if (patient.id == patientId)
-                {
-                    foreach (Report report in patient.reports)
-                    {
-                        if (report.id == ReportId)
-                            patient.reports.Remove(report);
-                    }
-                }
-            }
+            
         }
     }
 }
--- Week_2/Week_2/DAL/patientDal.cs
+++ Week_2/Week_2/DAL/patientDal.cs
@@ -2,11 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Week_2.Models;
 
 namespace Week_2.DAL
 {
     public class patientDal
     {
+
+        //A function that return all reports.
+        public static List<Report> GetAllReports()
+        {
+
+            List<Report> allReports = new List<Report>();
+            foreach (Patient patient in Data.Data.patients)
+            {
+                allReports.AddRange(patient.reports);
+            }
+            return allReports;
+        }
+
+
+        //A function that get idPatient and return all his reports.
+        public static List<Report> GetReportsByIdPatient(int id)
+        {
+            foreach (Patient patient in Data.Data.patients)
+            {
+                if (patient.id == id)
+                    return patient.reports;
+            }
+            return null;
+        }
+
+        // A function tat get city and return all reports un thus city.
+        public static List<Report> GetReportsByCity(string city)
+        {
+            List<Report> reports = new List<Report>();
+            foreach (Patient patient in Data.Data.patients)
+            {
+                foreach (Report report in patient.reports)
+                {
+                    if (report.city == city)
+                        reports.Add(report);
+                }
+            }
+            return reports;
+
+        }
+
+
+        // A function that get report of one patient and add it to this patient reports.
+        public static void AddReport(Report report, int patientId)
+        {
+            bool patientExit = false;
+            foreach (Patient patient in Data.Data.patients)
+            {
+                if (patient.id == patientId)
+                {
+                    patient.reports.Add(report);
+                    patientExit = true;
+                }
+            }
+            if (patientExit == false)
+            {
+                Patient newPatient = new Patient(new List<Report> { report }, patientId);
+                Data.Data.patients.Add(newPatient);
+            }
+        }
 
     }
 }
